e98c6a6ec46e5c3f0e4219e2fb04e444f94c97c3
Leyton Valencia <leytonvalencia.6190@hotmail.com>
2023-09-13 22:52:25 -0500

    Creation/structure/base [Leyton Valencia] Se agrega la pangina de detalles de publicaciones

--- FiverrTemplate/src/pages/gig/Gig.jsx
+++ FiverrTemplate/src/pages/gig/Gig.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Gig.scss'
 import {Slider} from "infinite-react-carousel/lib";
+import FolderTree, { testData } from "react-folder-tree";
+
+
+
+
+const BasicTree = () => {
+  const onTreeStateChange = (state) => console.log("tree state: ", state);
+
+  return <FolderTree data={testData} onChange={onTreeStateChange} />;
+};
 
 const Gig = () => {
+
   return (
     <div className="gig">
       <div className="container">
@@ -21,45 +32,20 @@ const Gig = () => {
               <span>5</span>
             </div>
           </div>
-          <Slider slidesToShow={1} arrowsScroll={1} className="slider">
-            <img
-              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-          </Slider>
-          <h2>About This Gig</h2>
-          <p>
-            I use an AI program to create images based on text prompts. This
-            means I can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things I've found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD character) Landscapes (E.g.
-            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
-            team, business, profile picture) You can be as vague or as
-            descriptive as you want. Being more vague will allow the AI to be
-            more creative which can sometimes result in some amazing images. You
-            can also be incredibly precise if you have a clear image of what you
-            want in mind. All of the images I create are original and will be
-            found nowhere else. If you have any questions you're more than
-            welcome to send me a message.
-          </p>
+          <div className="slider">
+            <BasicTree />
+          </div>
+
+          
           <div className="seller">
             <h2>About The Seller</h2>
             <div className="user">
               <img
-                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                src="https://fiverr-res.cloudinary.com/t_profile_original,q_auto,f_auto/attachments/profile/photo/025573035bbbb4f85290b4fb5667029c-1637071982769/800762d9-419d-4e18-a065-64d139377e15.jpg"
                 alt=""
               />
               <div className="info">
-                <span>Anna Bell</span>
+                <span>Steve Chacon</span>
                 <div className="stars">
                   <img src="/img/star.png" alt="" />
                   <img src="/img/star.png" alt="" />
@@ -68,38 +54,41 @@ const Gig = () => {
                   <img src="/img/star.png" alt="" />
                   <span>5</span>
                 </div>
-                <button>Contact Me</button>
+                <button>Ver Perfil</button>
               </div>
             </div>
             <div className="box">
               <div className="items">
                 <div className="item">
-                  <span className="title">From</span>
-                  <span className="desc">USA</span>
+                  <span className="title">Pais</span>
+                  <span className="desc">Ecuador</span>
                 </div>
                 <div className="item">
-                  <span className="title">Member since</span>
-                  <span className="desc">Aug 2022</span>
+                  <span className="title">Miembro desde</span>
+                  <span className="desc">Agosto 2023</span>
                 </div>
                 <div className="item">
-                  <span className="title">Avg. response time</span>
-                  <span className="desc">4 hours</span>
+                  <span className="title">Numero de Publicaciones</span>
+                  <span className="desc">7</span>
                 </div>
                 <div className="item">
-                  <span className="title">Last delivery</span>
-                  <span className="desc">1 day</span>
+                  <span className="title">Ultima Conexion</span>
+                  <span className="desc">Hace 3 dias</span>
                 </div>
                 <div className="item">
-                  <span className="title">Languages</span>
-                  <span className="desc">English</span>
+                  <span className="title">Lenguajes</span>
+                  <span className="desc">Ingles, Ecuador</span>
                 </div>
               </div>
               <hr />
               <p>
-                My name is Anna, I enjoy creating AI generated art in my spare
-                time. I have a lot of experience using the AI program and that
-                means I know what to prompt the AI with to get a great and
-                incredibly detailed result.
+              Soy un desarrollador web de pila completa con una amplia 
+              gama de conocimientos en diferentes lenguajes de front-end 
+              y back-end, marcos receptivos, bases de datos y mejores 
+              prácticas de código. Aporto años de investigación y 
+              experiencia en la industria y puedo comprender los 
+              requisitos a fondo y crear el software requerido por 
+              el cliente de manera oportuna.
               </p>
             </div>
           </div>
@@ -234,44 +223,52 @@ const Gig = () => {
         </div>
         <div className="right">
           <div className="price">
-            <h3>1 AI generated image</h3>
-            <h2>$ 59.99</h2>
+            <h3>
+              Crearé un sitio web con transmisión en
+              vivo y la función de historia de Instagram.
+           </h3>
           </div>
           <p>
-            I will create a unique high quality AI generated image based on a
-            description that you give me
+          A partir de este servicio, creadores como YouTubers, 
+          Instagrammers, fotógrafos y muchos más comienzan a 
+          ganar dinero vendiendo su contenido a sus fans y 
+          seguidores. El modelo de trabajo es similar al de 
+          solo fans y sitios de Patreon.
           </p>
           <div className="details">
             <div className="item">
               <img src="/img/clock.png" alt="" />
-              <span>2 Days Delivery</span>
+              <span>Fecha Inicio: 09/04/2023</span>
             </div>
             <div className="item">
               <img src="/img/recycle.png" alt="" />
-              <span>3 Revisions</span>
+              <span>2 Actualizaciones</span>
             </div>
           </div>
           <div className="features">
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Prompt writing</span>
+              <span>NextJS</span>
             </div>
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Artwork delivery</span>
+              <span>JavaScript</span>
             </div>
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Image upscaling</span>
+              <span>MongoDB</span>
             </div>
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Additional design</span>
+              <span>Diseño UI</span>
             </div>
           </div>
-          <button>Continue</button>
+          <button>Contactar</button>
+          <button>Descargar</button>
         </div>
+        
       </div>
+      
     </div>
   )
 }
