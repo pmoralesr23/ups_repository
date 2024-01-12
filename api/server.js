40d0177151f9c896579aee59bf1575d37a339ab3
Leyton Valencia <76409855+leytonvalencia@users.noreply.github.com>
2024-01-11 23:17:11 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -26,7 +26,7 @@ const app = express()
       }
 }
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://upscodenest-cloud.preview-domain.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoute)
